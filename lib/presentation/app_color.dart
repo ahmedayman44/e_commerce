3e7e6a3877e4fccc1cd3929ffb77456734bb6cb7
ahmedayman44 <ahmedayman203011@gmail.com>
2025-09-01 14:54:20 +0300

    creating app color

---
+++ lib/presentation/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  //create color static  So that I, you, get it from his class without me doing instance  from it.
+
+  static Color primary = const Color(0XFFED9728);
+  static Color darkGrey = const Color(0XFF525252);
+  static Color grey = const Color(0XFF737477);
+  static Color lightGrey = const Color(0xff9E9E9E);
+}
