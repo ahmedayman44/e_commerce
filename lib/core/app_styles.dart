1363ee3270b4219bb05c792ca37d2f4af785cd01
ahmedayman44 <ahmedayman203011@gmail.com>
2025-09-02 12:59:25 +0300

    creating app styles

---
+++ lib/core/app_styles.dart
@@ -0,0 +1,51 @@
+import 'package:e_commerce/core/app_fonts.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTetStyle(double fontSize, Color color, FontWeight fontWeight) {
+  return TextStyle(
+    color: color,
+    fontFamily: FontConstants.fontFamily,
+    fontWeight: fontWeight,
+    fontSize: fontSize,
+  );
+}
+
+// Regular
+TextStyle getRegularStyle({
+  required Color color,
+  double fontSize = FontSizeManager.s14,
+}) {
+  return _getTetStyle(fontSize, color, FontWeightManager.regular);
+}
+
+// Bold
+TextStyle getBoldStyle({
+  required Color color,
+  double fontSize = FontSizeManager.s14,
+}) {
+  return _getTetStyle(fontSize, color, FontWeightManager.bold);
+}
+
+// SemiBold
+TextStyle getSemiBold({
+  required Color color,
+  double fontSize = FontSizeManager.s14,
+}) {
+  return _getTetStyle(fontSize, color, FontWeightManager.semiBold);
+}
+
+// light
+TextStyle getLightStyle({
+  required Color color,
+  double fontSize = FontSizeManager.s14,
+}) {
+  return _getTetStyle(fontSize, color, FontWeightManager.light);
+}
+
+// Meduim
+TextStyle getMeduimStyle({
+  required Color color,
+  double fontSize = FontSizeManager.s14,
+}) {
+  return _getTetStyle(fontSize, color, FontWeightManager.medium);
+}
