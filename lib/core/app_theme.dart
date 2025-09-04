d1792e379e97bd18ddc501ed8d960ed2ab744346
ahmedayman44 <ahmedayman203011@gmail.com>
2025-09-04 12:40:50 +0300

    creating app theme

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,110 @@
+import 'package:e_commerce/core/app_color.dart';
+import 'package:e_commerce/core/app_fonts.dart';
+import 'package:e_commerce/core/app_styles.dart';
+import 'package:e_commerce/core/app_values.dart';
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    // Main Color
+    primaryColor: AppColor.primary,
+    primaryColorLight: AppColor.lightPrimary,
+    primaryColorDark: AppColor.darkPrimary,
+    disabledColor: AppColor.grey1,
+    splashColor: AppColor.lightPrimary, // ripple effect color
+    // Card Theme
+    cardTheme: CardTheme(
+      color: AppColor.white,
+      elevation: AppSize.s4,
+      shadowColor: AppColor.grey,
+    ),
+
+    // APPBar Theme
+    appBarTheme: AppBarTheme(
+      color: AppColor.primary,
+      elevation: AppSize.s4,
+      centerTitle: true,
+      shadowColor: AppColor.lightPrimary,
+      titleTextStyle: getRegularStyle(
+        color: AppColor.white,
+        fontSize: FontSizeManager.s16,
+      ),
+    ),
+
+    // Button Theme
+    buttonTheme: ButtonThemeData(
+      shape: StadiumBorder(),
+      buttonColor: AppColor.primary,
+      disabledColor: AppColor.grey1,
+      splashColor: AppColor.lightPrimary,
+    ),
+
+    // elevated Button Theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(
+          color: AppColor.white,
+          fontSize: FontSizeManager.s17,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
+
+    // Thex Theme
+    textTheme: TextTheme(
+      headlineMedium: getSemiBold(
+        color: AppColor.darkGrey,
+        fontSize: AppSize.s16,
+      ),
+      titleMedium: getMeduimStyle(
+        color: AppColor.lightGrey,
+        fontSize: FontSizeManager.s14,
+      ),
+      bodyMedium: getRegularStyle(color: AppColor.grey1),
+      displayMedium: getLightStyle(
+        color: AppColor.white,
+        fontSize: FontSizeManager.s22,
+      ),
+    ),
+
+    // Input Decoration Theme (TextFormField)
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(AppPadding.p12),
+      hintStyle: getRegularStyle(
+        color: AppColor.grey,
+        fontSize: FontSizeManager.s14,
+      ),
+      labelStyle: getMeduimStyle(
+        color: AppColor.grey,
+        fontSize: FontSizeManager.s14,
+      ),
+      errorStyle: getRegularStyle(color: AppColor.error),
+
+      // enabled Border
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+        borderSide: BorderSide(color: AppColor.primary, width: AppSize.s1_5),
+      ),
+
+      // Focesed Border
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: AppColor.grey, width: AppSize.s1_5),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+
+      // Error Border
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: AppColor.error, width: AppSize.s1_5),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+
+      // Focus Error Border
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: AppColor.primary, width: AppSize.s1_5),
+        borderRadius: BorderRadius.all(Radius.circular(AppSize.s8)),
+      ),
+    ),
+  );
+}
--- lib/core/app_values.dart
+++ lib/core/app_values.dart
@@ -9,6 +9,7 @@ class AppMargin {
 }
 
 class AppPadding {
+  static const double p8 = 8.0;
   static const double p12 = 12.0;
   static const double p14 = 14.0;
   static const double p16 = 16.0;
@@ -19,6 +20,9 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s8 = 8.0;
+  static const double s1_5 = 1.5;
+  static const double s4 = 4.0;
   static const double s12 = 12.0;
   static const double s14 = 14.0;
   static const double s16 = 16.0;
